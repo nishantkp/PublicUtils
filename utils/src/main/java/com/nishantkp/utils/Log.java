cd39699b6f4bb9e308170fceffa4e3bd9fea40d4
Nishant Patel <nishantuwindsor@gmail.com>
2019-05-04 10:23:15 -0400

    Add support for feagment/activity/application inLlog

--- utils/src/main/java/com/nishantkp/utils/Log.java
+++ utils/src/main/java/com/nishantkp/utils/Log.java
@@ -1,5 +1,10 @@
 package com.nishantkp.utils;
 
+import android.app.Activity;
+import android.app.Application;
+
+import androidx.fragment.app.Fragment;
+
 /**
  * This class will print the log messages
  */
@@ -15,6 +20,42 @@ public class Log {
      */
     public static void ui(Class clazz, String id, String m) {
         android.util.Log.i(clazz.getSimpleName(), id + " : " + m);
+    }
+
+    /**
+     * Log with unique identifier
+     * ui -> Unique Identifier
+     *
+     * @param ac activity instance
+     * @param id unique id which easily distinguish log from rest of logs
+     * @param m  your log message
+     */
+    public static void ui(Activity ac, String id, String m) {
+        android.util.Log.i(ac.getClass().getSimpleName(), id + " : " + m);
+    }
+
+    /**
+     * Log with unique identifier
+     * ui -> Unique Identifier
+     *
+     * @param app application class instance
+     * @param id  unique id which easily distinguish log from rest of logs
+     * @param m   your log message
+     */
+    public static void ui(Application app, String id, String m) {
+        android.util.Log.i(app.getClass().getSimpleName(), id + " : " + m);
+    }
+
+    /**
+     * Log with unique identifier
+     * ui -> Unique Identifier
+     *
+     * @param fr class instance
+     * @param id unique id which easily distinguish log from rest of logs
+     * @param m  your log message
+     */
+    public static void ui(Fragment fr, String id, String m) {
+        android.util.Log.i(fr.getClass().getSimpleName(), id + " : " + m);
     }
 
     /**
